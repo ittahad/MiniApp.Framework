14019ba98b302493fded34533a1a8122956908f2
ittahadzaman <ittahad.zaman@selise.ch>
2022-07-14 15:31:12 +0600

    test

--- UnitTests/TokenServiceTests.cs
+++ UnitTests/TokenServiceTests.cs
@@ -22,5 +22,39 @@ namespace UnitTests
             // Assert
             Assert.NotNull(token);
         }
+
+        [Fact]
+        public void Test2()
+        {
+            // Arrange
+            TokenService tokenService = new();
+            string key = "42342342342324524wrertertertertert";
+            string issuer = "testfdg";
+            string audience = "testdfg";
+            string username = "testdfgs";
+
+            // Act
+            var token = tokenService.BuildToken(key, issuer, audience, username);
+
+            // Assert
+            Assert.NotNull(token);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            // Arrange
+            TokenService tokenService = new();
+            string key = "sdfjkshdfjshdfkjshdjkfhsdf";
+            string issuer = "sdfsdf";
+            string audience = "tessdftdfg";
+            string username = "sdfsdf";
+
+            // Act
+            var token = tokenService.BuildToken(key, issuer, audience, username);
+
+            // Assert
+            Assert.NotNull(token);
+        }
     }
 }
